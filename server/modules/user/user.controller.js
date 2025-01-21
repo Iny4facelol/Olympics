dc00a425a4fdf36c17cf692624b1364a091bf657
marina <antomar92@icloud.com>
2025-01-21 15:38:13 +0100

    addActivityToUser y responsableValidateDocument FUNCIONAN

--- server/modules/user/user.controller.js
+++ server/modules/user/user.controller.js
@@ -4,11 +4,11 @@ import { completeResponsibleSchema } from "../../utils/zodSchemas/completeRespon
 import { registerSchema } from "../../utils/zodSchemas/registerSchema.js";
 import { z } from "zod";
 import userDal from "./user.dal.js";
- 
+
 class UserController {
   register = async (req, res) => {
     const parsedData = registerSchema.parse(req.body);
- 
+
     try {
       const {
         user_name,
@@ -24,11 +24,11 @@ class UserController {
         user_password,
         user_center_id,
       } = parsedData;
- 
+
       // Validación de campos
- 
+
       const hash = await hashPassword(user_password);
- 
+
       const values = [
         user_name,
         user_lastname,
@@ -43,11 +43,10 @@ class UserController {
         hash,
         user_center_id,
       ];
- 
+
       await userDal.register(values);
       res.status(200).json({ msg: "Usuario registrado correctamente" });
     } catch (error) {
-
       if (error instanceof z.ZodError) {
         res.status(400).json({ error: error.errors });
       } else {
@@ -55,19 +54,19 @@ class UserController {
       }
     }
   };
- 
+
   login = async (req, res) => {
     const { user_email, user_password } = req.body;
- 
+
     try {
       const result = await userDal.getUserByEmail(user_email);
- 
+
       if (result.length === 0) {
         res.status(401).json({ message: "datos incorrectos" });
       } else {
         const user = result[0];
         const match = await comparePassword(user_password, user.user_password);
- 
+
         if (match) {
           const token = generateToken(user.user_id);
           res.status(200).json({ token, user });
@@ -79,23 +78,17 @@ class UserController {
       res.status(500).json({ message: "Error en el servidor" });
     }
   };
- 
+
   completeCenter = async (req, res) => {
     try {
-
-      const {
-        center_city,
-        center_province,
-        center_address,
-        center_phone,
-      } = req.body;
- 
+      const { center_city, center_province, center_address, center_phone } =
+        req.body;
 
       const { filename } = req.file;
       const center_auth_doc = filename;
- 
+
       const { center_id } = req.params;
- 
+
       if (
         !center_city ||
         !center_province ||
@@ -107,7 +100,7 @@ class UserController {
           "Todos los campos son requeridos para completar el centro."
         );
       }
- 
+
       const result = await userDal.completeCenter({
         center_id,
         center_city,
@@ -116,32 +109,24 @@ class UserController {
         center_phone,
         center_auth_doc,
       });
- 
+
       return res.status(200).json({ message: "Centro completado con éxito." });
     } catch (error) {
       console.log(error);
       return res.status(400).json({ message: error.message });
     }
   };
- 
+
   completeResponsible = async (req, res) => {
     const parsedData = completeResponsibleSchema.parse(req.body);
- 
-    try {
 
-      const {
-        user_name,
-        user_lastname,
-        user_dni,
-        user_phone,
-        user_password,        
-      } = parsedData;
- 
+    try {
+      const { user_name, user_lastname, user_dni, user_phone, user_password } =
+        parsedData;
 
       const { user_id } = req.params;
- 
-      const hash = await hashPassword(user_password);
 
+      const hash = await hashPassword(user_password);
 
       const values = [
         user_name,
@@ -156,26 +141,25 @@ class UserController {
       res.status(200).json({ msg: "Responsable completado con éxito." });
     } catch (error) {
       if (error instanceof z.ZodError) {
-
         res.status(400).json({ error: error.errors });
       } else {
         res.status(400).json({ message: error.message });
       }
     }
   };
- 
+
   editResponsible = async (req, res) => {
     try {
       const { user_id } = req.params;
       const { user_name, user_lastname, user_phone, user_dni } = req.body;
- 
+
       const result = await userDal.updateResponsible(user_id, {
         user_name,
         user_lastname,
         user_phone,
         user_dni,
       });
- 
+
       return res.status(200).json({
         message: "Responsable actualizado con éxito.",
         result,
@@ -188,7 +172,7 @@ class UserController {
       });
     }
   };
- 
+
   editCenter = async (req, res) => {
     try {
       const { id } = req.params;
@@ -199,7 +183,7 @@ class UserController {
         center_phone,
         center_auth_doc,
       } = req.body;
- 
+
       if (
         !center_city ||
         !center_province ||
@@ -211,7 +195,7 @@ class UserController {
           "Todos los campos son requeridos para editar el centro."
         );
       }
- 
+
       const result = await userDal.updateCenter(id, {
         center_city,
         center_province,
@@ -219,7 +203,7 @@ class UserController {
         center_phone,
         center_auth_doc,
       });
- 
+
       return res
         .status(200)
         .json({ message: "Centro actualizado con éxito.", result });
@@ -230,7 +214,7 @@ class UserController {
       });
     }
   };
- 
+
   editUserUser = async (req, res) => {
     try {
       const { id } = req.params;
@@ -245,7 +229,7 @@ class UserController {
         user_phone,
         user_birth_date,
       } = req.body;
- 
+
       if (
         !id ||
         !user_name ||
@@ -262,7 +246,7 @@ class UserController {
           message: "Todos los campos son requeridos para editar el usuario.",
         });
       }
- 
+
       const result = await userDal.updateUserUser(id, {
         user_name,
         user_lastname,
@@ -274,7 +258,7 @@ class UserController {
         user_phone,
         user_birth_date,
       });
- 
+
       return res
         .status(200)
         .json({ message: "Usuario actualizado con éxito.", result });
@@ -284,58 +268,41 @@ class UserController {
         .json({ message: "Error al actualizar usuario.", error });
     }
   };
- 
+
   ResponsibleValidateDocument = async (req, res) => {
     try {
       const { user_id } = req.params;
-      const { user_is_validated } = req.body;
-      const documento = req.file; // Archivo subido
- 
-      if (!user_id || user_is_validated === undefined) {
-        return res.status(400).json({
-
-          message:
-            "El ID del usuario y el estado de validación son requeridos.",
-
-        });
-      }
- 
-      if (!documento) {
-        return res.status(400).json({ message: "El documento es requerido." });
-      }
- 
+      
       const result = await userDal.updateDocumentValidation(
         user_id,
-        user_is_validated
       );
 
-      return res
+      res
         .status(200)
         .json({ message: "Documento validado con éxito.", result });
-
     } catch (error) {
-      console.log("Error al validar documento:", error);
-      return res
+   
+      res
         .status(500)
-        .json({ message: "Error al validar documento.", error });
+        .json({error });
     }
   };
- 
+//REVISADO CON SANTI
   addActivityToUser = async (req, res) => {
     try {
       const { user_id } = req.params;
       const { activity_id, center_id, olympics_id } = req.body;
- 
+
       console.log("Body:", req.body);
       console.log("Params:", req.params);
- 
+
       if (!user_id || !activity_id || !center_id || !olympics_id) {
         return res.status(400).json({
           message:
             "Todos los campos son requeridos: user_id, activity_id, center_id, olympics_id",
         });
       }
- 
+
       // Este bloque simula la inserción
       const result = {
         user_id,
@@ -344,7 +311,7 @@ class UserController {
         olympics_id,
         message: "Actividad añadida al usuario con éxito.",
       };
- 
+
       return res.status(200).json(result);
     } catch (error) {
       console.log("Error al añadir actividad al usuario:", error);
@@ -354,5 +321,5 @@ class UserController {
     }
   };
 }
- 
+
 export default new UserController();
