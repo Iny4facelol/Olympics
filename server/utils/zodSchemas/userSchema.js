e2ba2534a228c4a3887a5e3bb3a758799a246700
virginia <fernandezcassovirginia@gmail.com>
2025-01-30 15:32:23 +0100

    añado funcionalidad al formulario de contacto para recibir emails

--- server/utils/zodSchemas/userSchema.js
+++ server/utils/zodSchemas/userSchema.js
@@ -173,3 +173,17 @@ export const passwordSchema = z
   message: "Las contraseñas no coinciden",
   path: ["user_confirm_password"],
 });
+
+export const contactSchema = z.object({
+  user_email: z.string().email("El email no es válido"),
+  user_name: z
+    .string()
+    .min(2, "El nombre debe tener al menos 2 caracteres")
+    .max(50, "El nombre no puede tener más de 50 caracteres"),
+  user_message: z
+    .string()
+    .min(10, { message: "La descripción debe tener al menos 10 caracteres" })
+    .max(255, {
+      message: "La descripción no puede tener más de 255 caracteres",
+    }),
+});
